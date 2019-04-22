d8a1385ce61eaa0da080dd245a0363698c17878c
Victor Fernando Matos da Silva <victorfm@lnxcit012244cps.cit>
2019-04-22 13:43:56 -0300

    Initial commit

---
+++ src/MutilplosThree.java
@@ -0,0 +1,9 @@
+// @autor Victorfmsilva
+public class MutilplosThree {
+    public static void main(String[] args) {
+        for(int i=0; i<100; i++){
+            if(i%3 == 0)
+                System.out.println( i+ " é multiplo de 3");
+        }
+    }
+}
